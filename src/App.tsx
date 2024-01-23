9ae1b10fe708975d8dc0d17b0b8f59b4cb8b8e99
Mohammed Aman Mev <mohammedaman0731@gmail.com>
2024-01-23 23:13:42 +0530

    lazy imprementation in pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 // import React from 'react';
 
+import { lazy } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./Pages/Home";
-import Search from "./Pages/Search";
-import Navbar from "./Components/Navbar";
+
+const Home = lazy(() => import("./Pages/Home"));
+const Search = lazy(() => import("./Pages/Search"));
+const Navbar = lazy(() => import("./Components/Navbar"));
+const Shipping = lazy(() => import("./Pages/Shipping"));
+const Order = lazy(() => import("./Pages/Order"));
+const Payment = lazy(() => import("./Pages/Payment"));
+const Cart = lazy(() => import("./Pages/Cart"));
 
 const App = () => {
   return (
@@ -12,6 +18,10 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/searchproduct" element={<Search />} />
+        <Route path="/shipping" element={<Shipping />} />
+        <Route path="/order" element={<Order />} />
+        <Route path="/payment" element={<Payment />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
     </BrowserRouter>
   );
