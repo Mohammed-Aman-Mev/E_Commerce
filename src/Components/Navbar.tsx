24058f389744e5baf966de00c6ff4c7a1c3630ea
Mohammed Aman Mev <mohammedaman0731@gmail.com>
2024-02-02 16:35:52 +0530

    logo

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,11 +1,19 @@
 const Navbar = () => {
   return (
     <nav className="w-full bg-orange-500 flex items-center justify-between px-5 py-3">
-      <div>LOGO</div>
+      <div>
+        <h3 className="font-bold text-2xl">LOGO</h3>
+      </div>
       <div className="flex px-3 justify-between items-center w-96">
-        <button className="px-4 py-2 bg-orange-400 rounded-md font-bold">Payments</button>
-        <button className="px-4 py-2 bg-orange-400 rounded-md font-bold">shipping</button>
-        <button className="px-4 py-2 bg-orange-400 rounded-md font-bold">carts</button>
+        <button className="px-4 py-2 bg-orange-400 rounded-md font-bold">
+          Payments
+        </button>
+        <button className="px-4 py-2 bg-orange-400 rounded-md font-bold">
+          shipping
+        </button>
+        <button className="px-4 py-2 bg-orange-400 rounded-md font-bold">
+          carts
+        </button>
       </div>
     </nav>
   );
