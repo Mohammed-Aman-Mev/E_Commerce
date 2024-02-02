8226a20179721eb4ad467fe662d8fd1be05a70f3
Mohammed Aman Mev <mohammedaman0731@gmail.com>
2024-02-02 16:44:26 +0530

    footer styling and hover button created

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,5 +1,9 @@
 const Footer = () => {
-  return <footer>footer</footer>;
+  return (
+    <footer className="w-full bg-black flex items-center justify-center">
+      <h1 className="text-white font-bold text-5xl py-5">footer</h1>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -5,13 +5,13 @@ const Navbar = () => {
         <h3 className="font-bold text-2xl">LOGO</h3>
       </div>
       <div className="flex px-3 justify-between items-center w-96">
-        <button className="px-4 py-2 bg-orange-400 rounded-md font-bold">
+        <button className="px-4 py-2 bg-orange-400 rounded-md font-bold hover:bg-orange-600 hover:text-white">
           Payments
         </button>
-        <button className="px-4 py-2 bg-orange-400 rounded-md font-bold">
+        <button className="px-4 py-2 bg-orange-400 rounded-md font-bold hover:bg-orange-600 hover:text-white">
           shipping
         </button>
-        <button className="px-4 py-2 bg-orange-400 rounded-md font-bold">
+        <button className="px-4 py-2 bg-orange-400 rounded-md font-bold hover:bg-orange-600 hover:text-white">
           carts
         </button>
       </div>
