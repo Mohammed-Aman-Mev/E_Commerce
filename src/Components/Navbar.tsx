ed5efff1d1155462aaca5c36a2bae7f283768c1e
Mohammed Aman Mev <mohammedaman0731@gmail.com>
2024-02-02 16:29:21 +0530

    navbar structired ready

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,5 +1,14 @@
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="w-full bg-orange-400 flex items-center justify-between px-5 py-7">
+      <div>LOGO</div>
+      <div className="flex px-3 justify-between items-center">
+        <button className=""></button>
+        <button className="">shipping</button>
+        <button className="">carts</button>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
